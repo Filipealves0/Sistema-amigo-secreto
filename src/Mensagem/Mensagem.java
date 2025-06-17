110ecf2604e47ab45aef8cac530084dde0f5a447
Filipealves0 <filipe.alves@dcx.ufpb.br>
2025-06-17 14:48:40 -0300

    Criação de classes Amigo e Mensagem

---
+++ src/Mensagem/Mensagem.java
@@ -0,0 +1,34 @@
+package Mensagem;
+
+public class Mensagem {
+    private String texto;
+    private String emailRemetente;
+    private Boolean anonima;
+
+    public Mensagem(){
+
+    }
+    public Mensagem(String emailRemetente, String texto, Boolean anonima) {
+        this.emailRemetente = emailRemetente;
+        this.texto = texto;
+        this.anonima = anonima;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getEmailRemetente() {
+        return emailRemetente;
+    }
+
+    public void setEmailRemetente(String emailRemetente) {
+        this.emailRemetente = emailRemetente;
+    }
+
+ 
+}
